b1bc22c73767163c70dce64e19af4460e76f1a91
Jatinthakur0202 <jatinthakur0202@gmail.com>
2022-07-05 00:00:59 +0530

    mine_game_2

--- game.py
+++ game.py
@@ -550,7 +550,161 @@ def sanidhya():   #increase PlayerA.coin acc to game
     pass
 
 def bhargav():
-    pass
+    def intro():
+        print("                It is impossible to win in this still you can try you best XD ")
+        print("                       Enter board number in you turn to play your move           ")
+        print("                     +_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+")
+        time.sleep(5)
+        os.system('cls')
+
+    board = {1: ' ', 2: ' ', 3: ' ',
+            4: ' ', 5: ' ', 6: ' ',
+            7: ' ', 8: ' ', 9: ' '}
+    pp = 'O'
+    computer = 'X'
+    #we impliment minimac algo....
+    #reference from https://levelup.gitconnected.com/mastering-tic-tac-toe-with-minimax-algorithm-3394d65fa88f
+    #Mastering Tic-Tac-Toe with Minimax Algorithm in Python
+    intro()
+    def printBoard(board):
+        os.system('cls')
+        print(board[1] + " |" + board[2] + " |" + board[3])
+        print("-+-+-+-+-")
+        print(board[4] + " |" + board[5] + " |" + board[6])
+        print("-+-+-+-+-")
+        print(board[7] + " |" + board[8] + " |" + board[9])
+        print("\n")
+       
+
+    def spaceIsFree(position):
+        if board[position] == ' ':
+            return True 
+        return False 
+
+    def insertLetter(letter, position):
+        if spaceIsFree(position):
+            board[position] = letter 
+            printBoard(board)
+            if checkDraw():
+                print("Draw!")
+                time.sleep(2)
+                start1()
+                
+            if checkWin():
+                if letter == 'X':
+                    print("Bot wins!")
+                    time.sleep(2)
+                    start1()
+                    
+                else:
+                    print("player wins!")
+                    PlayerA.coins += 10
+                    time.sleep(2)
+                    start1()
+            return 
+        else:
+            print("Invalid position")
+            position = int(input("Please enter a new position: "))
+            insertLetter(letter, position)
+            return    
+
+    def checkWin():
+        if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+            return True
+        elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+            return True
+        elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+            return True
+        elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+            return True
+        elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+            return True
+        elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+            return True
+        elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+            return True
+        elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
+            return True
+        else:
+            return False
+
+    def checkWhichMarkWon(mark):
+        if (board[1] == board[2] and board[1] == board[3] and board[1] == mark):
+            return True
+        elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
+            return True
+        elif (board[7] == board[8] and board[7] == board[9] and board[7] == mark):
+            return True
+        elif (board[1] == board[4] and board[1] == board[7] and board[1] == mark):
+            return True
+        elif (board[2] == board[5] and board[2] == board[8] and board[2] == mark):
+            return True
+        elif (board[3] == board[6] and board[3] == board[9] and board[3] == mark):
+            return True
+        elif (board[1] == board[5] and board[1] == board[9] and board[1] == mark):
+            return True
+        elif (board[7] == board[5] and board[7] == board[3] and board[7] == mark):
+            return True
+        else:
+            return False
+
+    def checkDraw():
+        for key in board.keys():
+            if board[key] == ' ':
+                return False 
+        return True 
+
+    def ppMove():
+        position = int(input("Enter a position for 'O': "))
+        insertLetter(pp, position)
+        return 
+
+    def compMove():
+        bestScore = -800
+        bestMove = 0
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = computer
+                score = minimax(board, False)
+                board[key] = ' '
+                if score > bestScore:
+                    bestScore = score 
+                    bestMove = key
+        insertLetter(computer, bestMove)
+        return 
+    def minimax(board, isMaximizing):
+        if checkWhichMarkWon(computer):
+            return 1 
+        elif checkWhichMarkWon(pp):
+            return -1 
+        elif checkDraw():
+            return 0
+            
+        if isMaximizing:
+            bestScore = -800 
+            for key in board.keys():
+                if board[key] == ' ':
+                    board[key] = computer 
+                    score = minimax(board, False)
+                    board[key] = ' '
+                    if score > bestScore:
+                        bestScore = score
+            return bestScore 
+        else:
+            bestScore = 800 
+            for key in board.keys():
+                if board[key] == ' ':
+                    board[key] = pp 
+                    score = minimax(board, True)
+                    board[key] = ' '
+                    if score < bestScore:
+                        bestScore = score 
+            return bestScore
+
+
+    while not checkWin():
+        compMove()
+        ppMove()
 
 def jatin():
   
